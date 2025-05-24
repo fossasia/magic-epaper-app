66e7ea9e5e76c635c8a640521be69432300d6fdf
Vishveshwara Uthayakumaran <u.vishveshwara@gmail.com>
2025-05-24 19:14:33 +0530

    Updated filter name to threshold

--- lib/view/widget/image_list.dart
+++ lib/view/widget/image_list.dart
@@ -25,14 +25,14 @@ class _ImageList extends State<ImageList> {
       ImageProcessing.bwFalseFloydSteinbergDither: 'False Floyd-Steinberg',
       ImageProcessing.bwStuckiDither: 'Stucki',
       ImageProcessing.bwAtkinsonDither: 'Atkinson',
-      ImageProcessing.bwNoDither: 'None',
+      ImageProcessing.bwThresholdDither: 'Threshold',
       ImageProcessing.bwHalftoneDither: 'Halftone',
       ImageProcessing.bwrHalftone: 'Color Halftone',
       ImageProcessing.bwrFloydSteinbergDither: 'BWR Floyd-Steinberg',
       ImageProcessing.bwrFalseFloydSteinbergDither: 'BWR False Floyd-Steinberg',
       ImageProcessing.bwrStuckiDither: 'BWR Stucki',
       ImageProcessing.bwrTriColorAtkinsonDither: 'BWR Atkinson',
-      ImageProcessing.bwrNoDither: 'None'
+      ImageProcessing.bwrThresholdDither: 'Threshold',
     };
   }
 
