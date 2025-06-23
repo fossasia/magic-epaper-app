4aa0f05db2e44f80ab8612ebcc850501c93f39a8
Dien-Nhung Nguyen-Phu <kein@kienlab.com>
2025-06-23 14:25:43 +0700

    feat: waveform luts

--- lib/util/epd/driver/driver.dart
+++ lib/util/epd/driver/driver.dart
@@ -1,5 +1,18 @@
+import 'waveform.dart';
+import 'package:magic_epaper_app/util/protocol.dart';
+
 abstract class Driver {
   String get driverName;
   List<int> get transmissionLines;
   int get refresh;
+  int get vcomLut;
+  int get wwLut;
+  int get bwLut;
+  int get wbLut;
+  int get bbLut;
+  int get panelSetting;
+  int get pllControl;
+  WaveformList get waveforms;
+  Future<void> setlut(Protocol p, Waveform waveform);
+  Future<void> init(Protocol p);
 }
