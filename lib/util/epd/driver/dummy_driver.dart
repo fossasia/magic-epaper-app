d82bb27403259462b5e99e3eb400168eb2b3234a
Vishveshwara Uthayakumaran <u.vishveshwara@gmail.com>
2025-07-07 16:22:12 +0530

    fix common build

--- lib/util/epd/driver/dummy_driver.dart
+++ lib/util/epd/driver/dummy_driver.dart
@@ -2,9 +2,9 @@ import 'driver.dart';
 
 class DummyDriver extends Driver {
   @override
-  int get refresh => 0;
+  int get refresh => 0x12;
   @override
   get driverName => 'NA';
   @override
-  List<int> get transmissionLines => [];
-}
+  List<int> get transmissionLines => [0x10, 0x13];
+  }
