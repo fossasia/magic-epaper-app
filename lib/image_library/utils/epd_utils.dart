e07fbd05d1bc9a09aa4653c6a4dbe56089521a72
Vishveshwara <u.vishveshwara@gmail.com>
2025-08-14 19:30:07 +0530

    dart formatting

--- lib/image_library/utils/epd_utils.dart
+++ lib/image_library/utils/epd_utils.dart
@@ -5,10 +5,9 @@ import 'package:magicepaperapp/util/epd/waveshare_2in9.dart';
 
 class EpdUtils {
   static DisplayDevice getEpdFromMetadata(Map<String, dynamic>? metadata) {
-
     if (metadata == null || !metadata.containsKey('epdModel')) {
       return Gdey037z03();
-   }
+    }
 
     final String epdModel = metadata['epdModel']?.toString() ?? '';
 
@@ -18,7 +17,7 @@ class EpdUtils {
       case 'GDEY037T03':
         return Gdey037z03BW();
       case 'waveshare-2.9':
-        return Waveshare2in9(); 
+        return Waveshare2in9();
       default:
         return Gdey037z03();
     }
