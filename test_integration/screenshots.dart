0a398bb8f8820742afe8a4de5642163723a7f69d
Vishveshwara <u.vishveshwara@gmail.com>
2025-08-19 16:16:45 +0530

    Modified Screenshot workflow

--- test_integration/screenshots.dart
+++ test_integration/screenshots.dart
@@ -19,15 +19,27 @@ void main() {
       await tester.pumpAndSettle();
       await binding.takeScreenshot('1_display_selection');
 
+      final sideBar = find.byIcon(Icons.menu);
+      if (sideBar.evaluate().isNotEmpty) {
+        await tester.tap(sideBar);
+        await tester.pumpAndSettle();
+      }
+      await binding.takeScreenshot('2_sidebar');
+
+      final selectDisplay = find.byIcon(Icons.edit);
+      if (sideBar.evaluate().isNotEmpty) {
+        await tester.tap(selectDisplay);
+        await tester.pumpAndSettle();
+      }
+
       final waveshare = find.text('Waveshare NFC');
       if (waveshare.evaluate().isNotEmpty) {
-        print('Tapping on Waveshare NFC');
         await tester.tap(waveshare);
         await tester.pumpAndSettle();
       }
-      final Continue = find.text('Continue');
-      if (waveshare.evaluate().isNotEmpty) {
-        await tester.tap(Continue);
+      final continueButton = find.text('Continue');
+      if (continueButton.evaluate().isNotEmpty) {
+        await tester.tap(continueButton);
         await tester.pumpAndSettle();
       }
       await tester.pump(const Duration(seconds: 1));
@@ -38,13 +50,13 @@ void main() {
         await tester.pumpAndSettle();
       }
       await tester.pump(const Duration(seconds: 1));
-      await binding.takeScreenshot('2_image_editor');
+      await binding.takeScreenshot('3_filter_selection');
 
       final openEditorButton = find.text('Open Editor');
       await tester.tap(openEditorButton);
       await tester.pumpAndSettle();
       await tester.pump(const Duration(seconds: 1));
-      await binding.takeScreenshot('3_open_editor');
+      await binding.takeScreenshot('4_open_editor');
 
       final NavigatorState navigator = tester.state(find.byType(Navigator));
       navigator.pop();
@@ -54,7 +66,7 @@ void main() {
       await tester.tap(adjustButton);
       await tester.pumpAndSettle();
       await tester.pump(const Duration(seconds: 2));
-      await binding.takeScreenshot('4_adjust_image');
+      await binding.takeScreenshot('5_adjust_image');
 
       navigator.pop();
       await tester.pumpAndSettle();
@@ -69,7 +81,12 @@ void main() {
       await tester.enterText(inputField, 'fossasia');
       await tester.testTextInput.receiveAction(TextInputAction.done);
       await tester.pumpAndSettle();
-      await binding.takeScreenshot('5_barcode_screen');
+      await binding.takeScreenshot('6_barcode_screen');
+      await tester.pumpAndSettle();
+      final generateImage = find.text('Generate Image');
+      await tester.tap(generateImage);
+      await tester.pumpAndSettle(const Duration(seconds: 2));
+      await binding.takeScreenshot('7_generated_Barcode');
     });
   });
 }
