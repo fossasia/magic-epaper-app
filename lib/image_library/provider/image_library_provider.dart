82c90519baf4e262204f63de6683f2eb711dd9fe
Riccardo Venturini <Giordanobruno227@gmail.com>
2026-05-05 11:21:59 +0200

    Fix: Impossible to save images in library

--- lib/image_library/provider/image_library_provider.dart
+++ lib/image_library/provider/image_library_provider.dart
@@ -38,14 +38,20 @@ class ImageLibraryProvider extends ChangeNotifier {
 
   Future<void> _initializeDirectories() async {
     if (_magicEpaperDirectory == null) {
-      final externalDir = await getExternalStorageDirectory();
-      if (externalDir != null) {
-        final storageRoot = Directory('/storage/emulated/0');
-        _magicEpaperDirectory = Directory('${storageRoot.path}/MagicEpaper');
-      } else {
-        final fallbackDir = await getApplicationDocumentsDirectory();
-        _magicEpaperDirectory = Directory('${fallbackDir.path}/MagicEpaper');
+      String path;
+      try {
+        if (Platform.isAndroid) {
+          final externalDir = await getExternalStorageDirectory();
+          path = externalDir?.path ??
+              (await getApplicationDocumentsDirectory()).path;
+        } else {
+          path = (await getApplicationDocumentsDirectory()).path;
+        }
+      } catch (e) {
+        AppLogger.error('Error initializing directory path: $e');
+        path = (await getApplicationDocumentsDirectory()).path;
       }
+      _magicEpaperDirectory = Directory('$path/MagicEpaper');
       if (!await _magicEpaperDirectory!.exists()) {
         await _magicEpaperDirectory!.create(recursive: true);
       }
